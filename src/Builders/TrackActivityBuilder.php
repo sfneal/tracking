0e156d1f842ccc0026604ef34ee85c919e3f87b4
Stephen Neal <stephen.neal14@gmail.com>
2020-11-09 11:35:48 -0500

    REFORMAT todo

--- src/Builders/TrackActivityBuilder.php
+++ src/Builders/TrackActivityBuilder.php
@@ -7,6 +7,7 @@ use Domain\Users\Builders\Traits\WhereUser;
 use Sfneal\Builders\QueryBuilder;
 use Sfneal\Tracking\Builders\Traits\WhereModels;
 
+// todo: add use of User package
 class TrackActivityBuilder extends QueryBuilder implements WhereUserInterface
 {
     use WhereModels,
