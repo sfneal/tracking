12a98ab9d35608c547334c2740a4eb287fed4fe9
Stephen Neal <stephen.neal14@gmail.com>
2021-04-12 14:06:12 -0400

    ADD `modelAttributeAssertions()` method for executing model attirbute assertions

--- tests/MigrationsTest.php
+++ tests/MigrationsTest.php
@@ -2,12 +2,27 @@
 
 namespace Sfneal\Tracking\Tests;
 
+use Illuminate\Database\Eloquent\Model;
 use Sfneal\Tracking\Models\TrackAction;
 use Sfneal\Tracking\Models\TrackActivity;
 use Sfneal\Tracking\Models\TrackTraffic;
 
 class MigrationsTest extends TestCase
 {
+    /**
+     * Execute model assertions.
+     *
+     * @param array $data
+     * @param Model $model
+     * @return void
+     */
+    private function modelAttributeAssertions(array $data, Model $model): void
+    {
+        foreach ($data as $attribute => $value) {
+            $this->assertSame($value, $model->{$attribute});
+        }
+    }
+
     /** @test */
     public function track_action_table_is_accessible()
     {
@@ -25,9 +40,7 @@ class MigrationsTest extends TestCase
         $newAction = TrackAction::query()->find($action->getKey());
 
         // Assert model has expected values
-        $this->assertSame($data['action'], $newAction->action);
-        $this->assertSame($data['model_table'], $newAction->model_table);
-        $this->assertSame($data['model_key'], $newAction->model_key);
+        $this->modelAttributeAssertions($data, $newAction);
     }
 
     /** @test */
@@ -50,12 +63,7 @@ class MigrationsTest extends TestCase
         $newActivity = TrackActivity::query()->find($activity->getKey());
 
         // Assert model has expected values
-        $this->assertSame($data['user_id'], $newActivity->user_id);
-        $this->assertSame($data['route'], $newActivity->route);
-        $this->assertSame($data['description'], $newActivity->description);
-        $this->assertSame($data['model_table'], $newActivity->model_table);
-        $this->assertSame($data['model_key'], $newActivity->model_key);
-        $this->assertSame($data['request_token'], $newActivity->request_token);
+        $this->modelAttributeAssertions($data, $newActivity);
     }
 
     /** @test */
@@ -88,21 +96,6 @@ class MigrationsTest extends TestCase
         $newTraffic = TrackTraffic::query()->find($traffic->getKey());
 
         // Assert model has expected values
-        $this->assertSame($data['user_id'], $newTraffic->user_id);
-        $this->assertSame($data['session_id'], $newTraffic->session_id);
-        $this->assertSame($data['app_version'], $newTraffic->app_version);
-        $this->assertSame($data['app_environment'], $newTraffic->app_environment);
-        $this->assertSame($data['request_host'], $newTraffic->request_host);
-        $this->assertSame($data['request_uri'], $newTraffic->request_uri);
-        $this->assertSame($data['request_method'], $newTraffic->request_method);
-        $this->assertSame($data['request_payload'], $newTraffic->request_payload);
-        $this->assertSame($data['request_ip'], $newTraffic->request_ip);
-        $this->assertSame($data['request_token'], $newTraffic->request_token);
-        $this->assertSame($data['response_code'], $newTraffic->response_code);
-        $this->assertSame($data['response_time'], $newTraffic->response_time);
-        $this->assertSame($data['agent_platform'], $newTraffic->agent_platform);
-        $this->assertSame($data['agent_device'], $newTraffic->agent_device);
-        $this->assertSame($data['agent_browser'], $newTraffic->agent_browser);
-        $this->assertSame($data['time_stamp'], $newTraffic->time_stamp);
+        $this->modelAttributeAssertions($data, $newTraffic);
     }
 }
