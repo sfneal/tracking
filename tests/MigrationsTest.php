7bef9a0b60ae1abec0fc74fb8835c725a65e373a
Stephen Neal <stephen.neal14@gmail.com>
2021-04-12 13:40:00 -0400

    ADD `track_activity_table_is_accessible()` test method to `MigrationsTest`

--- tests/MigrationsTest.php
+++ tests/MigrationsTest.php
@@ -5,30 +5,57 @@ namespace Sfneal\Tracking\Tests;
 
 
 use Sfneal\Tracking\Models\TrackAction;
+use Sfneal\Tracking\Models\TrackActivity;
 
 class MigrationsTest extends TestCase
 {
     /** @test */
     public function track_action_table_is_accessible()
     {
-        // Expected
-        $actionText = 'Created the model.';
-        $model_table = 'people';
-        $model_key = 40;
+        // Expected data
+        $data = [
+            'action' => 'Created the model.',
+            'model_table' => 'people',
+            'model_key' => 40,
+        ];
 
-        // Save the `TrackAction`
-        $action = new TrackAction();
-        $action->action = $actionText;
-        $action->model_table = $model_table;
-        $action->model_key = $model_key;
-        $action->save();
+        // Create the `TrackAction`
+        $action = TrackAction::query()->create($data);
 
         // Retrieve the `TrackAction`
         $newAction = TrackAction::query()->find($action->getKey());
 
         // Assert model has expected values
-        $this->assertSame($actionText, $newAction->action);
-        $this->assertSame($model_table, $newAction->model_table);
-        $this->assertSame($model_key, $newAction->model_key);
+        $this->assertSame($data['action'], $newAction->action);
+        $this->assertSame($data['model_table'], $newAction->model_table);
+        $this->assertSame($data['model_key'], $newAction->model_key);
+    }
+
+    /** @test */
+    public function track_activity_table_is_accessible()
+    {
+        // Expected data
+        $data = [
+            'user_id' => 38,
+            'route' => 'people.store',
+            'description' => 'Created a new People model.',
+            'model_table' => 'people',
+            'model_key' => 22,
+            'request_token' => uniqid()
+        ];
+
+        // Create the `TrackActivity` model
+        $activity = TrackActivity::query()->create($data);
+
+        // Retrieve the new `TrackActivity` model
+        $newActivity = TrackActivity::query()->find($activity->getKey());
+
+        // Assert model has expected values
+        $this->assertSame($data['user_id'], $newActivity->user_id);
+        $this->assertSame($data['route'], $newActivity->route);
+        $this->assertSame($data['description'], $newActivity->description);
+        $this->assertSame($data['model_table'], $newActivity->model_table);
+        $this->assertSame($data['model_key'], $newActivity->model_key);
+        $this->assertSame($data['request_token'], $newActivity->request_token);
     }
 }
