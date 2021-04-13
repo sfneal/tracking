d5abab640f687df7a7d7d9602814b07ef89e5819
Stephen Neal <stephen.neal14@gmail.com>
2021-04-13 12:24:52 -0400

    START making `EventsTest`

---
+++ tests/Feature/EventsTest.php
@@ -0,0 +1,27 @@
+<?php
+
+
+namespace Sfneal\Tracking\Tests\Feature;
+
+
+use Sfneal\Tracking\Tests\EventFakerSetup;
+use Sfneal\Tracking\Tests\TestCase;
+
+class EventsTest extends TestCase
+{
+    use EventFakerSetup;
+
+    /** @test */
+    public function track_activity_event_can_be_fired()
+    {
+        // todo: finish building tests
+        $this->assertTrue(true);
+    }
+
+    /** @test */
+    public function track_traffic_event_can_be_fired()
+    {
+        // todo: finish building tests
+        $this->assertTrue(true);
+    }
+}
