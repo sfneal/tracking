bd08ec06634e72a296fdb73304876baa05e2c990
Stephen Neal <stephen.neal14@gmail.com>
2021-04-13 16:42:24 -0400

    ADD body to `track_activity_event_can_be_fired()` method
    ADD `MockModelsServiceProvider` & people migrations to `TestCase`.

--- tests/Feature/EventsTest.php
+++ tests/Feature/EventsTest.php
@@ -4,6 +4,8 @@ namespace Sfneal\Tracking\Tests\Feature;
 
 use Illuminate\Support\Facades\Event;
 use Illuminate\Support\Facades\Route;
+use Sfneal\Testing\Models\People;
+use Sfneal\Tracking\Events\TrackActivityEvent;
 use Sfneal\Tracking\Events\TrackTrafficEvent;
 use Sfneal\Tracking\Middleware\TrackTrafficMiddleware;
 use Sfneal\Tracking\Tests\CreateRequest;
@@ -36,8 +38,17 @@ class EventsTest extends TestCase
     /** @test */
     public function track_activity_event_can_be_fired()
     {
-        // todo: finish building tests
-        $this->assertTrue(true);
+        // Assert that no events have been pushed
+        Event::assertNotDispatched(TrackActivityEvent::class);
+
+        // Create a Model
+        $model = People::factory()->create();
+
+        // Dispatch the event
+        event(new TrackActivityEvent($model));
+
+        // Assert the Event was pushed
+        Event::assertDispatched(TrackActivityEvent::class);
     }
 
     /** @test */
