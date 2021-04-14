52882a773ba23a990e84d7be26c093c2644aa9b3
Stephen Neal <stephen.neal14@gmail.com>
2021-04-14 17:50:03 -0400

    MAKE `QueriesTestCase`

--- tests/Feature/Queries/QueriesTestCase.php
+++ tests/Feature/Queries/QueriesTestCase.php
@@ -1,9 +1,35 @@
 <?php
 
+
 namespace Sfneal\Tracking\Tests\Feature\Queries;
 
+
+use Illuminate\Support\Collection;
+use Sfneal\Tracking\Models\Base\Tracking;
 use Sfneal\Tracking\Tests\TestCase;
 
 class QueriesTestCase extends TestCase
 {
+    /**
+     * @var Tracking
+     */
+    public $modelClass = Tracking::class;
+
+    /**
+     * @var Collection
+     */
+    public $models;
+
+    /**
+     * Setup the test environment.
+     *
+     * @return void
+     */
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        // Retrieve the People model from an Address model
+        $this->models = $this->modelClass::factory()->count(1000)->create();
+    }
 }
