1623f7168e579d2a38bde59aad0f06c5fd7ee0c0
Stephen Neal <stephen.neal14@gmail.com>
2021-04-14 13:37:31 -0400

    ADD test methods to `TrackTrafficBuilderTest`

--- tests/Feature/Builders/TrackTrafficBuilderTest.php
+++ tests/Feature/Builders/TrackTrafficBuilderTest.php
@@ -13,4 +13,84 @@ class TrackTrafficBuilderTest extends BuilderTestCase
      * @var TrackTraffic
      */
     protected $modelClass = TrackTraffic::class;
+
+    /** @test */
+    public function whereRequestUri()
+    {
+        $expected = 1;
+
+        $request_uri = $this->modelClass::query()
+            ->distinct()
+            ->get('request_uri')
+            ->shuffle()
+            ->take($expected)
+            ->pluck('request_uri')
+            ->first();
+
+        $model = $this->modelClass::query()->whereRequestUri($request_uri)->get();
+
+        $this->assertContains($request_uri, $model->pluck('request_uri'));
+    }
+
+    /** @test */
+    public function whereRequestUriIn()
+    {
+        $expected = 4;
+
+        $request_uris = $this->modelClass::query()
+            ->distinct()
+            ->get('request_uri')
+            ->shuffle()
+            ->take($expected)
+            ->pluck('request_uri')
+            ->toArray();
+
+        $models = $this->modelClass::query()->whereRequestUriIn($request_uris)->get();
+
+        foreach ($request_uris as $request_uri) {
+            $this->assertContains($request_uri, $models->pluck('request_uri'));
+        }
+    }
+
+    /** @test */
+    public function whereEnvironment()
+    {
+        $expected = 1;
+
+        $app_environment = $this->modelClass::query()
+            ->distinct()
+            ->get('app_environment')
+            ->shuffle()
+            ->take($expected)
+            ->pluck('app_environment')
+            ->first();
+
+        $models = $this->modelClass::query()->whereEnvironment($app_environment)->get();
+
+        foreach ($models as $model) {
+            $this->assertSame($app_environment, $model->app_environment);
+        }
+    }
+
+    /** @test */
+    public function whereEnvironmentProduction()
+    {
+        $app_environment = 'production';
+        $models = $this->modelClass::query()->whereEnvironmentProduction()->get();
+
+        foreach ($models as $model) {
+            $this->assertSame($app_environment, $model->app_environment);
+        }
+    }
+
+    /** @test */
+    public function whereEnvironmentDevelopment()
+    {
+        $app_environment = 'development';
+        $models = $this->modelClass::query()->whereEnvironmentDevelopment()->get();
+
+        foreach ($models as $model) {
+            $this->assertSame($app_environment, $model->app_environment);
+        }
+    }
 }
