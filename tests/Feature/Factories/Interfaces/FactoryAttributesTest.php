d215253873d0905eb9480b6d43acb8643f3436f6
Stephen Neal <stephen.neal14@gmail.com>
2021-04-14 15:14:21 -0400

    REFACTOR `FactoryTest` interface in two separate interfaces

--- tests/Feature/Factories/Interfaces/FactoryAttributesTest.php
+++ tests/Feature/Factories/Interfaces/FactoryAttributesTest.php
@@ -5,11 +5,8 @@ namespace Sfneal\Tracking\Tests\Feature\Factories\Interfaces;
 
 
 // todo: consider moving to sfneal/mock-models?
-interface FactoryTest
+interface FactoryAttributesTest
 {
-    /** @test */
-    public function fillables_are_correct_types();
-
     /** @test */
     public function attributes_are_correct_types();
 }
---
+++ tests/Feature/Factories/Interfaces/FactoryFillablesTest.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace Sfneal\Tracking\Tests\Feature\Factories\Interfaces;
+
+
+// todo: consider moving to sfneal/mock-models?
+interface FactoryFillablesTest
+{
+    /** @test */
+    public function fillables_are_correct_types();
+}
