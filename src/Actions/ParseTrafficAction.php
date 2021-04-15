6a8037898ec3675695ee184b0cc9f2a37d264f40
Stephen Neal <stephen.neal14@gmail.com>
2021-04-15 13:58:41 -0400

    CUT use of `session_id()`
    REFORMAT todo.

--- src/Actions/ParseTrafficAction.php
+++ src/Actions/ParseTrafficAction.php
@@ -43,7 +43,9 @@ class ParseTrafficAction extends Action
     ) {
         // Initialize event for serialization
         $this->tracking['user_id'] = intval(auth()->id());
-        $this->tracking['session_id'] = Cookie::get('hpa_laravel_session') ?? session_id();
+
+        // todo: add config value
+        $this->tracking['session_id'] = Cookie::get('hpa_laravel_session');
         $this->tracking['app_version'] = AppInfo::version();
         $this->tracking['time_stamp'] = $this->getTimestamp($time_stamp);
 
