fe0578465067bf8896fe3ba3a20dfa1263fb5b3f
Stephen Neal <stephen.neal14@gmail.com>
2021-04-15 12:50:42 -0400

    MAKE `TrackTrafficTest` for testing `TrackTraffic` model functionality

---
+++ tests/Unit/TrackTrafficTest.php
@@ -0,0 +1,71 @@
+<?php
+
+
+namespace Sfneal\Tracking\Tests\Unit;
+
+
+use Sfneal\Tracking\Actions\TrackTrafficAction;
+use Sfneal\Tracking\Events\TrackTrafficEvent;
+use Sfneal\Tracking\Models\TrackTraffic;
+use Sfneal\Tracking\Tests\CreateRequest;
+use Sfneal\Tracking\Tests\CrudModelTest;
+use Sfneal\Tracking\Tests\TestCase;
+
+class TrackTrafficTest extends TestCase implements CrudModelTest
+{
+    use CreateRequest;
+
+    /** @test */
+    public function records_can_be_created()
+    {
+        $trackingData = (new TrackTrafficEvent(
+            $this->createRequest(),
+            response('OK'),
+            microtime(true)
+        ))->tracking;
+
+        $traffic = (new TrackTrafficAction($trackingData))->execute();
+
+        $this->assertInstanceOf(TrackTraffic::class, $traffic);
+        $this->assertEquals('testing', $traffic->app_environment);
+        $this->assertEquals('GET', $traffic->request_method);
+    }
+
+    /** @test */
+    public function records_can_be_updated()
+    {
+        $trackingData = (new TrackTrafficEvent(
+            $this->createRequest(),
+            response('OK'),
+            microtime(true)
+        ))->tracking;
+
+        $traffic = (new TrackTrafficAction($trackingData))->execute();
+        $traffic->update([
+            'app_environment' => 'development',
+            'request_method' => 'post',
+        ]);
+
+        $this->assertInstanceOf(TrackTraffic::class, $traffic);
+        $this->assertEquals('development', $traffic->app_environment);
+        $this->assertEquals('POST', $traffic->request_method);
+    }
+
+    /** @test */
+    public function records_can_be_deleted()
+    {
+        $trackingData = (new TrackTrafficEvent(
+            $this->createRequest(),
+            response('OK'),
+            microtime(true)
+        ))->tracking;
+
+        $traffic = (new TrackTrafficAction($trackingData))->execute();
+
+        $traffic->delete();
+
+        $this->assertInstanceOf(TrackTraffic::class, $traffic);
+        $this->assertTrue($traffic->wasDeleted());
+        $this->assertNull(TrackTraffic::query()->find($traffic->getKey()));
+    }
+}
