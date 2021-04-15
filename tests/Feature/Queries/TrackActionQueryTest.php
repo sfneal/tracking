1a39a3f47323b49e8e23d4939634b3233213a0e2
Stephen Neal <stephen.neal14@gmail.com>
2021-04-15 11:03:00 -0400

    ADD `query_with_table_param()` test method to `TrackActionQueryTest`

--- tests/Feature/Queries/TrackActionQueryTest.php
+++ tests/Feature/Queries/TrackActionQueryTest.php
@@ -25,7 +25,33 @@ class TrackActionQueryTest extends QueriesTestCase
         // Query Builder
         $builder = (new TrackActionQuery($request))->execute();
 
+        // Execute assertions
         $this->assertInstanceOf(TrackActionBuilder::class, $builder);
         $this->assertEquals($this->count, $builder->count());
+    }
+
+    /** @test */
+    public function query_with_table_param()
+    {
+        // Table name
+        $table = 'people';
+
+        // All of `TrackAction` records
+        $records = TrackAction::query()
+            ->where('model_table', '=', $table)
+            ->get();
+
+        // Create a request
+        $request = $this->createRequest([], [
+            'table' => $table
+        ]);
+
+        // Query Builder
+        $builder = (new TrackActionQuery($request))->execute();
+
+        // Execute assertions
+        $this->assertInstanceOf(TrackActionBuilder::class, $builder);
+        $this->assertEquals($records->count(), $builder->count());
+        $this->assertEquals($records, $builder->get());
     }
 }
