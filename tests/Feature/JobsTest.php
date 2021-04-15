292664b687d06ccbab95a0c779eeed6ed6af30bd
Stephen Neal <stephen.neal14@gmail.com>
2021-04-15 13:06:41 -0400

    CUT unneeded `TrackActivityJob`

--- tests/Feature/JobsTest.php
+++ tests/Feature/JobsTest.php
@@ -6,7 +6,6 @@ use Illuminate\Support\Facades\Queue;
 use Sfneal\Testing\Models\People;
 use Sfneal\Tracking\Jobs\CleanDevTrackingJob;
 use Sfneal\Tracking\Jobs\TrackActionJob;
-use Sfneal\Tracking\Jobs\TrackActivityJob;
 use Sfneal\Tracking\Tests\TestCase;
 
 class JobsTest extends TestCase
@@ -41,26 +40,5 @@ class JobsTest extends TestCase
 
         // Assert a job was pushed...
         Queue::assertPushed(TrackActionJob::class, 1);
-    }
-
-    /** @test */
-    public function track_activity_job_can_be_queued()
-    {
-        // Enable queue faking
-        Queue::fake();
-
-        // Assert that no jobs were pushed...
-        Queue::assertNothingPushed();
-
-        // Dispatch the first job...
-        Queue::push(new TrackActivityJob(
-            People::factory()->create(),
-            uniqid(),
-            random_int(1, 999),
-            '/'
-        ));
-
-        // Assert a job was pushed...
-        Queue::assertPushed(TrackActivityJob::class, 1);
     }
 }
