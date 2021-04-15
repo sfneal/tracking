025adc60be869bf26fcbb74c9d44de31ff898b49
Stephen Neal <stephen.neal14@gmail.com>
2021-04-15 10:59:14 -0400

    ADD params to `createRequest()` trait method

--- tests/CreateRequest.php
+++ tests/CreateRequest.php
@@ -11,11 +11,16 @@ trait CreateRequest
      * Create a Request to be used in test methods.
      *
      * @param array $headers
+     * @param array $parameters
+     * @param array $cookies
+     * @param array $files
+     * @param array $server
+     * @param null $content
      * @return Request
      */
-    protected function createRequest(array $headers = []): Request
+    protected function createRequest(array $headers = [], array $parameters = [], array $cookies = [], array $files = [], array $server = [], $content = null): Request
     {
-        $request = Request::create('/', 'GET', [], [], [], [], []);
+        $request = Request::create('/', 'GET', $parameters, $cookies, $files, $server, $content);
 
         foreach ($headers as $header => $value) {
             $request->headers->set($header, $value);
