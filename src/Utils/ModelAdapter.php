9599b5d76419f5400e98e9025b0ae3c0b3bc2b00
Stephen Neal <stephen.neal14@gmail.com>
2021-04-16 13:22:13 -0400

    FIX issue with `ModelAdapter` return types not being string
    MAKE `ModelAdapterTest` for testing that returns from `ModelAdapter` are the correct model instances.

--- src/Utils/ModelAdapter.php
+++ src/Utils/ModelAdapter.php
@@ -13,9 +13,9 @@ class ModelAdapter
     /**
      * Retrieve a `TrackAction` model set from the 'tracking' config.
      *
-     * @return TrackAction
+     * @return TrackAction|string
      */
-    public static function TrackAction(): TrackAction
+    public static function TrackAction(): string
     {
         return config('tracking.models.TrackAction', TrackAction::class);
     }
@@ -23,9 +23,9 @@ class ModelAdapter
     /**
      * Retrieve a `TrackActivity` model set from the 'tracking' config.
      *
-     * @return TrackActivity
+     * @return TrackActivity|string
      */
-    public static function TrackActivity(): TrackActivity
+    public static function TrackActivity(): string
     {
         return config('tracking.models.TrackActivity', TrackActivity::class);
     }
@@ -33,9 +33,9 @@ class ModelAdapter
     /**
      * Retrieve a `TrackTraffic` model set from the 'tracking' config.
      *
-     * @return TrackTraffic
+     * @return TrackTraffic|string
      */
-    public static function TrackTraffic(): TrackTraffic
+    public static function TrackTraffic(): string
     {
         return config('tracking.models.TrackTraffic', TrackTraffic::class);
     }
